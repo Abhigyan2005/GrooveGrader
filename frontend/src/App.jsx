4d67d8d7fca78bd99d9172698fcf4890ce8bb63d
Abhigyan2005 <abhigyanjha334@gmail.com>
2025-12-08 22:35:22 +0530

    dashboard ui and implement api calls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,37 +3,29 @@ import Login from "./components/Login";
 import { Link, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
 function App() {
-  return (
+  return (  
     <>
-      <div class="relative min-h-screen overflow-hidden flex justify-center items-center">
-        <div className="absolute inset-0 pointer-events-none">
+      <div class="relative min-h-screen overflow-x-hidden">
+        <div className="absolute inset-0 pointer-events-none z-6">
           <div className="absolute  -top-[10%] -left-[10%] w-[500px] h-[500px] bg-emerald-900/20 rounded-full blur-[120px]" />
           <div className="absolute  -bottom-[10%] -right-[10%] w-[500px] h-[500px] bg-purple-900/20 rounded-full blur-[120px]" />
         </div>
 
-        <div className="relative z-10 w-full max-w-[700px] px-6">
+        <div className="relative z-20 w-full">
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <div className="min-h-screen w-screen flex justify-center items-center">
+                  <div className="relative z-5 w-full max-w-[700px] px-6">
+                    <Login />
+                  </div>
+                </div>
+              }
+            />
             <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </div>
-
-        {/* GitHub icon */}
-        <a
-          href="https://github.com/Abhigyan2005"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="fixed top-4 right-4 w-10 h-10 text-white hover:text-gray-300"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            className="w-full h-full"
-          >
-            <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.387.6.11.82-.26.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.082-.73.082-.73 1.205.084 1.84 1.238 1.84 1.238 1.07 1.835 2.805 1.305 3.49.998.108-.775.418-1.305.762-1.605-2.665-.3-5.467-1.335-5.467-5.93 0-1.31.467-2.38 1.235-3.22-.125-.303-.535-1.523.115-3.176 0 0 1.005-.322 3.3 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.28-1.552 3.282-1.23 3.282-1.23.653 1.653.243 2.873.12 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.807 5.625-5.48 5.92.43.37.815 1.096.815 2.21 0 1.595-.015 2.882-.015 3.27 0 .32.21.694.825.576C20.565 21.795 24 17.297 24 12c0-6.63-5.373-12-12-12z" />
-          </svg>
-        </a>
       </div>
     </>
   );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,47 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import disk from "../assets/vinyl.svg";
 
 function Dashboard() {
   const [loading, setLoading] = useState(true);
-  const [userdata, setUserData] = useState(null);
+  const [Data, setData] = useState(null);
+
+  const messages = [
+    "Analyzing your questionable taste…",
+    "Looking for your top artist… hopefully it’s not Pitbull",
+    "Roasting your playlists… gently.",
+  ];
+
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    const getUserData = async () => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % messages.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
       try {
-        const userprofile = await axios.get('http://127.0.0.1:8080/api/profile',{withCredentials: true});
-        setUserData({
-          profile: userprofile.data
+        const [userProfile, userArtists, userTracks] = await Promise.all([
+          axios.get("http://127.0.0.1:8080/api/profile", {
+            withCredentials: true,
+          }),
+          axios.get("http://127.0.0.1:8080/api/artists", {
+            withCredentials: true,
+          }),
+          axios.get("http://127.0.0.1:8080/api/tracks", {
+            withCredentials: true,
+          }),
+        ]);
+
+        setData({
+          profile: userProfile.data,
+          artist: userArtists.data,
+          tracks: userTracks.data,
         });
       } catch (error) {
         console.log(error);
@@ -18,23 +49,106 @@ function Dashboard() {
         setLoading(false);
       }
     };
-    getUserData();
+
+    getData();
   }, []);
 
   if (loading) {
-    return <div className='w-full h-screen bg-amber-300'>loading bitch</div>;
+    return (
+      <div className="min-h-screen w-screen flex flex-col gap-4 justify-center items-center bg-black">
+        <div className="w-12 h-12 border-8 border-green-500 rounded-full border-t-transparent animate-spin"></div>
+        <div key={index} className="text-lg text-white fade">
+          {messages[index]}
+        </div>
+      </div>
+    );
   }
 
-  if (!userdata || !userdata.profile) {
-    return <div className='w-full h-screen bg-red-500'>failed to load</div>;
+  if (!Data || !Data.profile) {
+    return <Navigate to="/" />;
   }
 
   return (
     <>
-      <img src={userdata.profile.images[0].url} alt="" />
-      <div className="text-white">{userdata.profile.display_name}</div>
+      <div className="flex flex-col w-screen items-center h-screen text-white">
+        <div className="w-[95%]">
+          <div className="flex justify-between items-center w-full pl-6 pr-6 pt-1 pb-1 bg-black/60 rounded-2xl mt-10">
+            <div className="flex justify-center items-center gap-4">
+              <img
+                src={Data.profile.images[0].url}
+                alt="profile"
+                className="w-17 h-17 rounded-full object-cover"
+              />
+              <div className="text-white">{Data.profile.display_name}</div>
+            </div>
+            <div className="text-green-400 hover:drop-shadow-[0_0_20px_white] cursor-pointer hover:scale-120 transition-transform duration-290">
+              Try Again!!
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row gap-10 w-full mt-5 ">
+            <div className="flex flex-col w-full md:w-[50%] bg-black/50 rounded-2xl h-auto p-5">
+              <div>
+                {/* basic metre */}
+              </div>
+              <div>
+                {/* gemini roast */}
+              </div>
+            </div>
+            <div className="flex flex-col  w-full md:w-[50%] bg-black/50 rounded-2xl h-auto p-5">
+              <div className="flex gap-4 pt-4 pb-4">
+                <img src={disk} alt="" />
+                <div className="font-bold text-3xl">The Evidence</div>
+              </div>
+              <div>
+                <div className="text-gray-400 font-bold mb-2">TOP ARTISTS</div>
+                <div className="flex flex-wrap gap-4">
+                  {Data.artist.items.slice(0, 8).map((artist) => {
+                    return (
+                      <div
+                        key={artist.id}
+                        className="relative group flex gap- items-center hover:scale-120 transition-transform duration-290"
+                      >
+                        <img
+                          src={artist.images[0].url}
+                          className="w-30 h-30 object-cover"
+                        />
+                        <div
+                          className="absolute inset-0 bg-black/70 flex items-center justify-center text-white text-center transition-opacity duration-300
+                opacity-100 md:opacity-0 group-hover:opacity-100"
+                        >
+                          {artist.name}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <div className="text-gray-400 font-bold mb-2 mt-3">
+                  TOP TRACKS
+                </div>
+
+                <div className="flex flex-col gap-3">
+                {Data.tracks.items.slice(0, 5).map((track) => {
+                  return (
+                    <div key={track.id} className="flex gap-5 items-center bg-primary">
+                      <img src={track.album.images[0].url} className="w-20 h-20 " alt="" />
+                      <div>
+                        <div>{track.name}</div>
+                        <div className="text-gray-500">{track.artists[0].name}</div>
+                      </div>
+                    </div>
+                  );
+                })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
-export default Dashboard
+export default Dashboard;
