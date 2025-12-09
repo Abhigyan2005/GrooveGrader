4e7bd067278a128fbc0703ec448866db2819ac79
Abhigyan2005 <abhigyanjha334@gmail.com>
2025-12-09 18:24:05 +0530

    fix port and add script for start in package.json

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,35 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import spotifyauth from "./routes/spotify.routes.js"
-import geminiAPI from "./routes/gemini.routes.js"
+import express from "express";
+import dotenv from "dotenv";
+import spotifyauth from "./routes/spotify.routes.js";
+import geminiAPI from "./routes/gemini.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(
-    cors({
-        origin: "https://groove-grader.vercel.app",
-        credentials: true,
-    })
-)
+  cors({
+    origin: "https://groove-grader.vercel.app",
+    credentials: true,
+  })
+);
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send('hello world')
-})
+  res.send("hello world");
+});
 
-app.use('/api', spotifyauth);
-app.use('/gemini', geminiAPI);
+// ROUTES
+app.use("/api", spotifyauth);
+app.use("/gemini", geminiAPI);
 
 
-app.listen(8080);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
